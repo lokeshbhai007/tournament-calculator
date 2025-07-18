0bb85bed66009ba7f4322582dbf8ded39e6f9cd7
lokeshbhai007 <lokeshmondal340@gmail.com>
2025-07-18 20:21:22 +0530

    add favi icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,19 +4,22 @@ import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import Toggle from "@/components/Toggle";
 import Providers from "./providers";
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from './api/auth/[...nextauth]/route'; // Now this import will work
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]/route"; // Now this import will work
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ranger.AI",
   description: "Tournament Point Calculator",
+  icons: {
+    icon: "/ranger-logo.jpg", // or "/favicon.ico" if you converted it
+  },
 };
 
 export default async function RootLayout({ children }) {
   const session = await getServerSession(authOptions);
-     
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -46,9 +49,7 @@ export default async function RootLayout({ children }) {
             <Sidebar />
             <div className="flex-1 md:ml-56">
               <Toggle />
-              <main className="main-content p-4">
-                {children}
-              </main>
+              <main className="main-content p-4">{children}</main>
             </div>
           </div>
         </Providers>
